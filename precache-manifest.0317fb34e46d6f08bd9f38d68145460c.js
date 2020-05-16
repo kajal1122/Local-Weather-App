1bebcc11f7b4f52ce6cc67e6b8ecdebbf5949c2e
Kajal Tiwari <kajal.lightbulb22@gmail.com>
2020-05-16 16:21:13 +0530

    version-v1

---
+++ precache-manifest.0317fb34e46d6f08bd9f38d68145460c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c36203f85e58473943eb043c5f4c9d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f92cc942d1efc7be04b4",
+    "url": "/static/css/main.8df3f4fe.chunk.css"
+  },
+  {
+    "revision": "860fea7984eb0ee08554",
+    "url": "/static/js/2.5fc139f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5fc139f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92cc942d1efc7be04b4",
+    "url": "/static/js/main.7008050a.chunk.js"
+  },
+  {
+    "revision": "817d2bf81990a1d39ee2",
+    "url": "/static/js/runtime-main.6537f79a.js"
+  }
+]);
